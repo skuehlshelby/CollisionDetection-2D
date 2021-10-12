d75718f6a3590f5e9334d74b30b86eba8d556309
Scott K <georgelarry99@gmail.com>
2021-10-12 15:00:35 +0200

    Added resize support.

---
+++ CollisionDetection.View.Web/ScaleChildWithParent.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace CollisionDetection.View.Web
+{
+    public class ScaleChildWithParent
+    {
+        private readonly ElementReference child;
+        private readonly IJSRuntime jsRuntime;
+        private readonly DotNetObjectReference<ScaleChildWithParent> objectReference;
+
+        private ScaleChildWithParent(ElementReference child, IJSRuntime jsRuntime)
+        {
+            this.child = child;
+            this.jsRuntime = jsRuntime;
+            objectReference = DotNetObjectReference.Create(this);
+        }
+
+        public static async Task<ScaleChildWithParent> Create(ElementReference child, ElementReference parent, IJSRuntime jsRuntime)
+        {
+            var instance = new ScaleChildWithParent(child, jsRuntime);
+            var size = await instance.GetSize(parent);
+            instance.SetSize(child, size.Height, size.Width);
+            instance.AddResizeListener(parent);
+            return instance;
+        }
+
+        public event EventHandler<Size> OnResize;
+
+        [JSInvokable]
+        public async void Resized(int height, int width)
+        {
+            await SetWidth(child, width);
+
+            OnResize?.Invoke(this, await GetSize(child));
+        }
+
+        private async void AddResizeListener(ElementReference element)
+        {
+            await jsRuntime.InvokeVoidAsync("addResizeListener", objectReference, element);
+        }
+
+        private async void SetSize(ElementReference element, int height, int width )
+        {
+            await jsRuntime.InvokeVoidAsync("setSize", element, height, width);
+        }
+
+        private async Task<Size> GetSize(ElementReference element)
+        {
+            return new Size(await GetWidth(element), await GetHeight(element));
+        }
+
+        private async Task<int> GetHeight(ElementReference element)
+        {
+            return await jsRuntime.InvokeAsync<int>("getHeight", element);
+        }
+
+        private async Task<int> GetWidth(ElementReference element)
+        {
+            return await jsRuntime.InvokeAsync<int>("getWidth", element);
+        }
+
+        private async ValueTask SetHeight(ElementReference element, int height)
+        {
+            await jsRuntime.InvokeVoidAsync("setHeight", element, height);
+        }
+
+        private async ValueTask SetWidth(ElementReference element, int width)
+        {
+            await jsRuntime.InvokeVoidAsync("setWidth", element, width);
+        }
+    }
+}
--- CollisionDetection.View.Web/wwwroot/js/script.js
+++ CollisionDetection.View.Web/wwwroot/js/script.js
@@ -13,3 +13,40 @@ function getSimulationWidth() {
 function setCanvasWidth(width) {
     document.getElementById("simulationCanvas").querySelector("canvas").width = width;
 }
+
+//New
+
+function addResizeListener(objectReference, element) {
+    window.addEventListener("resize", () => objectReference.invokeMethodAsync("Resized", getHeight(element), getWidth(element)));
+}
+
+function getHeight(element) {
+    return Math.floor(element.getBoundingClientRect().height);
+}
+
+function getWidth(element) {
+    return Math.floor(element.getBoundingClientRect().width);
+}
+
+function setSize(element, height, width) {
+    element.height = height;
+    element.width = width;
+}
+
+function setHeight(element, height) {
+    element.height = height;
+}
+
+function setWidth(element, width) {
+    element.width = width;
+}
+
+function isOverflownVertically(elementId) {
+    var element = document.getElementById(elementId);
+    return element.scrollHeight > element.clientHeight;
+}
+
+function isOverflownHorizontally(elementId) {
+    var element = document.getElementById(elementId);
+    return element.scrollWidth > element.clientWidth;
+}
